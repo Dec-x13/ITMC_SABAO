bdfbe3e143f2e843e5d415de39f961d9fd29c8a6
crab061904 <carlbrandws@gmail.com>
2025-05-20 21:30:57 +0800

    routes for cards done right

--- SABAO/src/components/card/Card.jsx
+++ SABAO/src/components/card/Card.jsx
@@ -1,25 +1,22 @@
-import "./Card.css";
+import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Card.css";
 
-
-function Card({book}) {
-    return (
-        
-        <Link key = {book.id}to="/story/1"className = "cardWrapper">
-            <div className = "infoWrapper">
-                <h3 className = "status">Releasing</h3>
-                <h2 className = "title">{book.title}</h2>
-                <h3 className = "desc">{book.description}</h3>
-                <h3 className = "branches">Total Branches: 100</h3>
-                <h3 className = "genre">{book.genres.join(', ')}</h3>
-            </div>
-            <div className = "storyImage">
-                <img src={book.coverImage} alt="" />
-            </div>
-        </Link>
-        
-        
-    )
+function Card({ book }) {
+  return (
+    <Link to={`/story/${book.id}`} className="cardWrapper" key={book.id}>
+      <div className="infoWrapper">
+        <h3 className="status">Releasing</h3>
+        <h2 className="title">{book.title}</h2>
+        <h3 className="desc">{book.description}</h3>
+        <h3 className="branches">Total Branches: 100</h3>
+        <h3 className="genre">{book.genres.join(', ')}</h3>
+      </div>
+      <div className="storyImage">
+        <img src={book.coverImage} alt={book.title} />
+      </div>
+    </Link>
+  );
 }
 
 export default Card;
