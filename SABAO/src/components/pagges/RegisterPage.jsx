ed745de9052a5ed17e89091e9b1c14ab7377208b
t0y0ta23 <152826889+t0y0ta23@users.noreply.github.com>
2025-05-21 15:32:54 +0800

    routingcj

---
+++ SABAO/src/components/pagges/RegisterPage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./pagges/styles.css";
+
+function RegisterPage() {
+  return (
+    <>
+      <div className="navbar">
+        <img src="/assets/SW_logo.png" alt="StoryWeaver Logo" className="logo" />
+        <span className="brand">
+          Story<span style={{ color: "#0055ff" }}>Weaver</span>
+        </span>
+      </div>
+      <div className="container">
+        <h1>Register an Account</h1>
+        <form method="POST" action="/register">
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter Username"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            required
+          />
+          <button type="submit">Create Account</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default RegisterPage;
